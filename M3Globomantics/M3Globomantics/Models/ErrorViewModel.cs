6a25d4bc045737558bc5a304721b34b36ce39027
gelu@codeindustry.net <gelu@codeindustry.net>
2023-04-01 15:28:38 +0300

    changing apps

--- M3Globomantics/M3Globomantics/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace M3Globomantics.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
